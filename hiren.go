c93413348c044ba4f9e96b500b02a46f9339b157
Ashutosh Das <git.pyprism@gmail.com>
2017-03-01 20:54:43 +0600

    added main funC

---
+++ hiren.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	//	"os"
+)
+
+func main() {
+	file, e := ioutil.ReadFile("./config.json")
+	if e != nil {
+		fmt.Printf("File error: %v\n", e)
+		fmt.Println(e.Error())
+		//log.Fatal(e)
+		//os.Exit(1)
+	}
+	var jsonobject jsontype
+	fmt.Printf("%s\n", string(file))
+	files, err := ioutil.ReadDir("/home/prism/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		fmt.Println(f.Name())
+		fmt.Println(f.IsDir())
+		fmt.Println(f.Size())
+	}
+}
+
+// package main
+//
+// import (
+// 	"fmt"
+// 	"path/filepath"
+// )
+//
+// func main() {
+// 	files, _ := filepath.Glob("*")
+// 	fmt.Println(files) // contains a list of all files in the current directory
+// }
